cc870aab2814998009f1ce5a345094454b75a83c
cristian-herrera <cherrera@activeit.cl>
2017-07-24 16:18:01 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function obtenerClima(){
+	var city = document.getElementById("idCiudad").value;
+	URL = "api.openweathermap.org/data/2.5/weather?q=" + city;
+	alert(URL);
+	var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
+    xmlhttp.open("POST", URL, false);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
+    xmlhttp.send(ItemJSON);
+    alert(xmlhttp.responseText);
+}
+
+function callbackFunction(xmlhttp) {
+    alert(xmlhttp.responseXML);
+}
